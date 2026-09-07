a8e871a19abc7dd3a27788c5448977547880362a
renuasmitha <renuasmitha2007@gmail.com>
2026-09-07 19:34:38 +0530

    Time: 18 ms (77.24%), Space: 108.2 MB (47.11%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet <Integer> hash= new HashSet<>();
+        for(int i : nums){
+            if(hash.contains(i)){
+                return true;
+            }
+            else {
+                hash.add(i);
+            }
+        }
+        return false;
+
+    }
+}
