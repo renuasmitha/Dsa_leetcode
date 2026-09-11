a20d12d25249c772f6edaa5ff90ec51abb25235a
renuasmitha <renuasmitha2007@gmail.com>
2026-09-11 20:05:04 +0530

    Time: 5 ms (63.03%), Space: 44.3 MB (82.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,47 +1,22 @@
 import java.util.HashMap;
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int n1=s.length();
-        int n2=t.length();
+   
+if(s.length()!=t.length()){
+    return false;
+}
+int[] count=new int[26];
+for(int i=0;i<s.length();i++){
+    count[s.charAt(i)-'a']++;
+    count[t.charAt(i)-'a']--;
 
-        HashMap<Character,Integer> count=new HashMap<>();
-        HashMap<Character,Integer> count2=new HashMap<>();
-        if(n1==n2)
-        {
-        for(int i=0;i<n1;i++){
-            if(count.containsKey(s.charAt(i)))
-            {
-                count.put(s.charAt(i),count.get(s.charAt(i))+1);
-            }
-            else{
-                count.put(s.charAt(i),1);
-            }
+}
+for(int j=0;j<26;j++){
+    if(count[j]!=0){
+        return false;
+    }
 
-        }
-        for(int i=0;i<n1;i++){
-            if(count2.containsKey(t.charAt(i)))
-            {
-                count2.put(t.charAt(i),count2.get(t.charAt(i))+1);
-            }
-            else{
-                count2.put(t.charAt(i),1);
-            }
-
-        }
-        if(count.equals(count2))
-        {
-            return true;
-        }
-        else{
-            return false;
-        }
-
-
-        }
-        else{
-            return false;
-        }
-
-
+}
+return true;
     }
 }
